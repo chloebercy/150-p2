8f5206fceda08a90179078d9aeaa180e34a3c14e
Chloe Bercy <chloebercy@Chloes-MacBook-Air-6.local>
2023-02-15 18:32:21 -0800

    now no found errors in uthread.c by -Wall -Werror

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -11,60 +11,74 @@
 #include "queue.h"
 
 struct uthread_tcb {
-	/*ucontext_t uthread_next;
-	stack_t uthread_stack;
-	mcontext_t uthread_registers;*/
-	uthread_ctx_t uthread_ctx; 
-	int state; // needed?
+	uthread_ctx_t *uthread_ctx; 
+	//int state;  
 };
 
-uthread_tcb **current = NULL;
+struct uthread_tcb **current = NULL; 
 queue_t *q = NULL;
 
 struct uthread_tcb *uthread_current(void)
 {
-	/* TODO Phase 2/3 */
 	return *current;
+	/* ADD TODO Phase 3 */
 }
 
 void uthread_yield(void)
 {
+	// set ctx_prev pointer to current context
+	uthread_ctx_t *uthread_ctx_prev = uthread_current()->uthread_ctx;
+
+	// set ctx_next pointer to next item in queue & update current
 	uthread_ctx_t *uthread_ctx_next;
 	queue_dequeue(*q, (void**)&uthread_ctx_next);
-	uthread_ctx_switch(uthread_current()->uthread_ctx, 
-		uthread_ctx_next); // will trigger uthread_ctx_next execution
+	(*current)->uthread_ctx = uthread_ctx_next;
+
+	// swapcontext()
+	uthread_ctx_switch(uthread_ctx_prev, uthread_ctx_next); // will trigger uthread_ctx_next execution
 }
 
 void uthread_exit(void)
 {
-	uthread_ctx_destroy_stack(uthread_current()->uthread_ctx->uc_stack);
-	queue_delete(*q, uthread_current->uthread_ctx);
-	free(**current); // free uthread_tcb struct pointed to by current
-	current = NULL;  
+	// destroy current tcb stack
+	uthread_ctx_destroy_stack(&(uthread_current()->uthread_ctx->uc_stack));
+
+	//queue_delete(*q, uthread_current()->uthread_ctx); should not be in queue
+
+	// free context pointed to by current
+	free(uthread_current()->uthread_ctx); 
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	uthread_tcb *newThread = (uthread_tcb*)malloc(sizeof(uthread_tcb));
-
+	// create newThread, initalize stack, makecontext(), add to queue;
+	uthread_ctx_t *newThread = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
+	
 	void *top_of_stack = uthread_ctx_alloc_stack();
-	uthread_ctx_init(newThread->uthread_ctx, top_of_stack, func, arg);
-
-	queue_enqueue(*q, newThread->uthread_ctx);
+	uthread_ctx_init(newThread, top_of_stack, func, arg);
+	queue_enqueue(*q, newThread);
 
 	return 0;
 }
 
 int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
-	// create uthread for main/idle and then create new queue
-	uthread_ctx_t *idle = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
+	// initialize global queue pointer q
 	*q = queue_create();
+	
+	// create uthread for main/idle 
+	uthread_ctx_t *idle = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
 
-	// create first thread
+	// set global var struct double pointer "current" to TCB of idle 
+	struct uthread_tcb *currentTCB = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
+	currentTCB->uthread_ctx = idle;
+	current = &currentTCB;
+
+	// create first thread & queue
 	uthread_create(func, arg);
+
+	// add idle to end of queue
 	queue_enqueue(*q, idle); 
-	// should we be just queining uthread_ctx_t types isntead of struct tcb? i think..
 
 	uthread_yield();
 	return 0;
