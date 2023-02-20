9d681d848ab7173b29b819589b9c98c7d092a469
Chloe Bercy <cxbercy@ucdavis.edu>
2023-02-19 20:28:00 -0800

    another tester pt2

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -19,8 +19,8 @@
  */
 #define HZ 100
 
-struct sigaction preempt_sa, previous_sa;
-struct itimerval previous_timer;
+struct sigaction *preempt_sa, *previous_sa;
+struct itimerval *timer, *previous_timer;
 sigset_t block_alarm;
 
 void timer_handler(int signum)
@@ -34,45 +34,59 @@ void timer_handler(int signum)
 
 void preempt_disable(void)
 {
-	sigprocmask (SIG_BLOCK, &block_alarm, NULL);
+	//sigprocmask(SIG_BLOCK, &block_alarm, NULL);
 	printf("preempt_disable\n");
 }
 
 void preempt_enable(void)
 {
 	printf("preempt_enable\n");
-	sigprocmask (SIG_UNBLOCK, &block_alarm, NULL);
+	//sigprocmask(SIG_UNBLOCK, &block_alarm, NULL);
+}
+
+void preempt_check(void){
+	struct itimerval check;
+	getitimer(ITIMER_VIRTUAL, &check);
+	printf("check = %ld\n", check.it_value.tv_usec);
+	setitimer(ITIMER_VIRTUAL, timer, NULL);
 }
 
 void preempt_start(bool preempt)
 {
 	printf("preempt gonna start!\n");
-	memset (&preempt_sa, 0, sizeof(preempt_sa));
-	//sigemptyset(&preempt_sa.sa_mask);
-    //preempt_sa.sa_flags = 0;
-    preempt_sa.sa_handler = timer_handler;
-	sigaction(SIGVTALRM, &preempt_sa, &previous_sa);
+	preempt_sa = (struct sigaction*)malloc(sizeof(struct sigaction));
+	previous_sa = (struct sigaction*)malloc(sizeof(struct sigaction));
+	timer = (struct itimerval*)malloc(sizeof(struct itimerval));
+	previous_timer = (struct itimerval*)malloc(sizeof(struct itimerval));
+
+	//memset (&preempt_sa, 0, sizeof(preempt_sa));
+	sigemptyset(&(preempt_sa->sa_mask));
+    preempt_sa->sa_flags = 0;
+    preempt_sa->sa_handler = timer_handler;
+	sigaction(SIGVTALRM, preempt_sa, previous_sa);
 
 	sigemptyset (&block_alarm);
   	sigaddset (&block_alarm, SIGVTALRM);
 
-	struct itimerval timer;
-  	timer.it_interval.tv_usec = 100; //1000000 / HZ;
-  	timer.it_interval.tv_sec = 0;
-  	timer.it_value.tv_usec = 100; //1000000 / HZ;
-  	timer.it_value.tv_sec = 0;
+  	timer->it_interval.tv_usec = (long int) 1000; //(long int) 10000 / HZ; //1000000
+  	timer->it_interval.tv_sec = 0;
+  	timer->it_value.tv_usec = (long int) 1000; // (long int) 10000 / HZ;
+  	timer->it_value.tv_sec = 0;
+
+	printf("check?? = %ld\n", timer->it_value.tv_usec);
 
 	if (preempt){
-		int check = setitimer(ITIMER_VIRTUAL, &timer, &previous_timer);
+		int check = setitimer(ITIMER_VIRTUAL, timer, previous_timer);
 		fprintf(stderr, "started timer? %d\n", check);
+		preempt_check();
 	} else {
-		getitimer(ITIMER_VIRTUAL, &previous_timer);
+		getitimer(ITIMER_VIRTUAL, previous_timer);
 	}		
 }
 
 void preempt_stop(void)
 {
-	setitimer(ITIMER_VIRTUAL, &previous_timer, NULL);
-	sigaction(SIGVTALRM, &previous_sa, NULL);
+	setitimer(ITIMER_VIRTUAL, previous_timer, NULL);
+	sigaction(SIGVTALRM, previous_sa, NULL);
 }
 
